9f5d9a8af2b7f97c887528f3f9240e8b6e5a0ba8
Clayton <claytonmarinho@gmail.com>
2020-02-15 20:20:47 +0100

    run npm audit fix

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import { renderToStaticMarkup as render } from 'react-dom/server';
-import expect from 'expect';
+import React from "react";
+import { renderToStaticMarkup as render } from "react-dom/server";
+import expect from "expect";
 
-import MainComponent from 'src/';
+import MainComponent from "src/";
 
-describe('Component', () => {
-	let props;
+describe("Component", () => {
+  let props;
 
-	beforeEach(() => {
-		props = {
-			content: [
-				{
-					text: 'text a',
-					className: 'test',
-					animation: 'fade',
-				},
-			],
-			time: 5000,
-			startDelay: 0,
-		};
-	});
+  beforeEach(() => {
+    props = {
+      content: [
+        {
+          text: "text a",
+          className: "test",
+          animation: "fade"
+        }
+      ],
+      time: 5000,
+      startDelay: 0
+    };
+  });
 
-	it('render component', () => {
-		expect(render(<MainComponent {...props} />))
-			.toContain('<span class="test" style="transition:opacity 500ms ease-in;opacity:0">text a</span>');
-	});
+  it("render component", () => {
+    expect(render(<MainComponent {...props} />)).toContain(
+      '<span class="test" style="transition:opacity 500ms ease-in;opacity:0">text a</span>'
+    );
+  });
 });
