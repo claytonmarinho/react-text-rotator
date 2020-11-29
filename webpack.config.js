dcf1b12a6c2c88d464aceae2a7f36d8bf9dd3d70
Clayton <claytonmarinho@gmail.com>
2020-11-29 18:49:16 +0100

    include webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = (env) => ({
+  entry: path.resolve(process.cwd(), "src/index.js"),
+  target: "node",
+  devtool: env.env === "dev" ? "inline-source-map" : undefined,
+  mode: env.env === "dev" ? "development" : "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, /lib/, /demo/],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              [
+                "@babel/plugin-transform-runtime",
+                {
+                  regenerator: true,
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js"],
+    modules: ["node_modules"],
+  },
+  output: {
+    path: path.resolve(process.cwd(), "lib"),
+    filename: "index.js",
+  },
+});
